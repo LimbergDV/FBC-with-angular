4f5eb8d9d73eaa3a0bfc774a5fc9d003aa10b9e2
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2025-03-15 18:07:07 -0600

    FCM inicializating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"goair-fcm","appId":"1:485424992997:web:4a5a74b1b941a2ea093a0c","storageBucket":"goair-fcm.firebasestorage.app","apiKey":"AIzaSyCbitWscKqUdRWe9tlo1lO3C2kpB128Hew","authDomain":"goair-fcm.firebaseapp.com","messagingSenderId":"485424992997"})),
+    provideMessaging(() => getMessaging())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
